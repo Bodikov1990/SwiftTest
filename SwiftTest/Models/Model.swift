f49bae5bf72d9671fdeb7cc4d12183aeb06b2fbb
Bodikov Kuat <kuat.bodikov@gmail.com>
2022-01-03 17:53:46 +0500

    Some changes

---
+++ SwiftTest/Models/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  SwiftTest
+//
+//  Created by Kuat Bodikov on 03.01.2022.
+//
+
+import Foundation
+
+struct Question {
+    var title: String
+    
+}
---
+++ SwiftTest/Services/DataManager.swift
@@ -0,0 +1,9 @@
+//
+//  DataManager.swift
+//  SwiftTest
+//
+//  Created by Kuat Bodikov on 03.01.2022.
+//
+
+import Foundation
+
